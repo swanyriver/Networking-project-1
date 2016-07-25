2c55a4c37665db97f0709f14d0d94decf93a9d2a
Brandon Swanson <swanyriver@gmail.com>
2016-07-24 19:45:58 -0700

    paramatarized client

--- relayChat/chatclient.py
+++ relayChat/chatclient.py
@@ -1,19 +1,23 @@
 import sys
 import socket
 
+REC_BUFFER = 512
+MSG_LIMIT = 500
+MIN_PORT = 0
+PRIVILEGED = 1024
+MAX_PORT = 65536
+QUIT = "\quit"
+
+
 def getInput(user):
     inpt = raw_input('%s>'%user)
+    if len(inpt) > MSG_LIMIT:
+        inpt = inpt[:MSG_LIMIT-2] + "\n"
     return inpt.strip()
 
-def main(argv):
-    #todo replace with safe parsing
-    #specify host name and port number on the command line.
-    TCP_IP = argv[0]
-    TCP_PORT = int(argv[1])
-    BUFFER_SIZE = 512
-    QUIT="\quit"
 
-    print "HOST:%s PORT:%d"%(TCP_IP, TCP_PORT)
+def client_main(TCP_IP, TCP_PORT):
+    #print "HOST:%s PORT:%d"%(TCP_IP, TCP_PORT)
 
     # create socket and connect to server specified in command line
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,8 +25,6 @@ def main(argv):
     print "peerName:", s.getpeername()
     #s.settimeout(.2)
 
-    userHandle="player1"
-    #TODO determine if verticalClient or horizontalClient
 
     msg = ""
 
@@ -32,7 +34,7 @@ def main(argv):
         s.sendall(msg)
 
         #get update from server
-        serverResponse = s.recv(BUFFER_SIZE)
+        serverResponse = s.recv(REC_BUFFER)
         print serverResponse
 
 
@@ -41,4 +43,16 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) < 2:
+        print "must supply host and port number"
+        sys.exit(1)
+
+    port = None
+    try:
+        port = int(sys.argv[2])
+        if not PRIVILEGED < port < MAX_PORT: raise ValueError("Out of range")
+    except ValueError:
+        print "Invalid port supplied"
+        sys.exit(1)
+
+    client_main(sys.argv[1], port)
